98085504d874e65b7645c607cb29eaf13a560891
LOVELY <kotagindeman@gmail.com>
2025-10-28 18:11:05 +0300

    up

--- js/screamer.js
+++ js/screamer.js
@@ -1,15 +1,3 @@
-// alert('привет скример')
-// console.log('привет скример');
-// const name = 'скример';
-// console.log(name);
-// // name = 'не скример'
-
-// let name = 'скример'
-// name = 'не скример'
-// console.log(name);
-
-// let name ;
-// console.log(name);
 
 const screamerCard = document.querySelector('#screamerCard')
 screamerCard.addEventListener('click', function() {
